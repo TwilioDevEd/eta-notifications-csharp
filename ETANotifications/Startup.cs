3467505359c2de517d805234d30ec4fb712a5fa4
nebtrx <neb.trx@gmail.com>
2016-02-24 09:51:08 -0500

    Initial commit

---
+++ ETANotifications/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ETANotifications.Startup))]
+namespace ETANotifications
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
